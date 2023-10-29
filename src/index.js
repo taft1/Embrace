0456f95783fa5e672194c22a65de9cd8bf18d661
Arsenii Khomei <arseniyhomey@gmail.com>
2023-10-28 19:43:37 -0500

    feat(EMBRACE-01): lazy loading

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import createRoot from 'react-dom'
-
+import React, { lazy, Suspense } from 'react'
+import { createRoot } from 'react-dom'
+import * as Sentry from '@sentry/react'
 import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 import App from './App'
 
-createRoot.render(
+const LazyApp = lazy(() => import('./App'))
+
+Sentry.init({
+  dsn: 'https://df70b1f6196f845ed07e7bbace374e6e@o4506130477547520.ingest.sentry.io/4506130483773440',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
   <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <LazyApp />
+    </Suspense>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  container
 )
