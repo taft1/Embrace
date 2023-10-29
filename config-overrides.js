2c2e16795a81f98f2a326b5f27f7b53044774e52
Arsenii Khomei <arseniyhomey@gmail.com>
2023-10-28 20:21:15 -0500

    feat(EMBRACE-01): image optimization

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+
+module.exports = function override(config, env) {
+    config.plugins.push(
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            disable: env !== 'production',
+        })
+    )
+
+    return config;
+}
